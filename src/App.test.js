ce94564971721a1105c72c47ff6bc076504ca370
shubhreetk <shubhreetk2005@gmail.com>
2026-04-08 01:29:01 +0530

    Fixed test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders dockerized react application text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Dockerized React Application/i);
+  expect(textElement).toBeInTheDocument();
 });
