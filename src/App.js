cdbe1b27153ac54c60c86392d7512c87c12cce7c
shubhreetk <shubhreetk2005@gmail.com>
2026-04-08 01:03:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Dockerized React Application</h1>
+      <p>Experiment 3.2.1 completed successfully</p>
+    </div>
+  );
+}
+
+export default App;
